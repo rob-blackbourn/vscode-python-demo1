5619cdbc98188618d3961fb02f3926932eb38594
Rob Blackbourn <rob.blackbourn@gmail.com>
2020-09-25 16:48:07 +0100

    More stuff

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,5 +6,3 @@ from vscode_python_demo1.server import echo_string
 def test_echo_string():
     """Test echo_string"""
     assert echo_string('Hello, World!') == 'Hello, World!'
-    foo = echo_string(1)
-    print(foo)
--- vscode_python_demo1/server.py
+++ vscode_python_demo1/server.py
@@ -4,8 +4,21 @@ from typing import List
 
 
 def echo_string(text: str) -> str:
+    """Echo the input argument
+
+    Args:
+        text (str): Some text
+
+    Returns:
+        str: The text provided
+    """
     return text
 
 
 def startup(_args: List[str]) -> None:
+    """Start the server
+
+    Args:
+        _args (List[str]): The command l;ine arguments.
+    """
     print(echo_string("Hello, World!"))
